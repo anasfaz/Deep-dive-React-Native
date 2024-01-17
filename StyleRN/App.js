e590ea1f452e6e6b046bd89fd67d94762a78476c
anasfaz <anaspkmmc@gmail.com>
2024-01-17 22:42:57 +0530

    StyleSheet API exmaple

--- StyleRN/App.js
+++ StyleRN/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native'
+
+//we can style different way 
+//one way inline Styling
+//another wau StyleSheet API
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>StyleSheet API</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  container:{flex:1,backgroundColor:"plum",padding:60}
+})
