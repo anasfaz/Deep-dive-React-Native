1de07be4f1cb1d8a9a883b9ade33421166d50b1b
anasfaz <anaspkmmc@gmail.com>
2024-01-20 17:00:51 +0530

    shadow group

--- StyleRN/App.js
+++ StyleRN/App.js
@@ -1,17 +1,38 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet } from "react-native";
 
-//we can style different way 
+//we can style different way
 //one way inline Styling
 //another wau StyleSheet API
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>StyleSheet API</Text>
+      <View style={[styles.box,styles.lightblueBg]}>
+        <Text>LIGHT BLUE</Text>
+      </View>
+      <View style={[styles.box,styles.lightgreenBg]}>
+        <Text>LIGHT GREEN</Text>
+      </View>
     </View>
-  )
+  );
 }
-
+//styling multiple using array syntax
 const styles = StyleSheet.create({
-  container:{flex:1,backgroundColor:"plum",padding:60}
-})
+  container: { flex: 1, backgroundColor: "plum", padding: 60 },
+  box: { width: '50%', height: '25%', padding: 10 },
+  lightblueBg: {
+    backgroundColor: "lightblue",
+  },
+  lightgreenBg: {
+    backgroundColor: "lightgreen",
+  },
+  boxShadow: {
+    shadowColor: '#333333',
+    shadowOffset: {
+      width: 6,
+      height:6
+    },
+    shadowOpacity: 0.6,
+    shadowRadius:4,
+  },
+});
