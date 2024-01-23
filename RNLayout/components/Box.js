0bc50c05ccb72b676fdb2a3df370bf4226bcbb69
anasfaz <anaspkmmc@gmail.com>
2024-01-24 00:49:35 +0530

    40 vedio compleated ✅

--- RNLayout/components/Box.js
+++ RNLayout/components/Box.js
@@ -9,7 +9,9 @@ export default function Box({children,style}) {
 const styles = StyleSheet.create({
     box: {
         backgroundColor: '#fff',
-        padding:20,
+        padding: 20,
+        width: 100,
+       height:100,
     },
     text: {
         fontSize: 24,
